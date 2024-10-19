8595854d1016619dbdf4dae3ca218f9f6606adc7
Sirawit Pratoomsuwan <48322519+bronystylecrazy@users.noreply.github.com>
2024-10-19 15:26:48 +0700

    Fix no default config created

--- strawhouse-command/common/config.go
+++ strawhouse-command/common/config.go
@@ -18,6 +18,13 @@ func InitConfig() {
 	viper.SetConfigName("command")
 	viper.SetConfigType("yaml")
 	viper.AddConfigPath(path)
+	if _, err := os.Stat(filepath.Join(path, "command.yaml")); os.IsNotExist(err) {
+		f, err := os.Create(filepath.Join(path, "command.yaml"))
+		if err != nil {
+			log.Fatalf("Failed to create config file: %v", err)
+		}
+		_ = f.Close()
+	}
 	if err := viper.ReadInConfig(); err != nil {
 		log.Fatalf("Failed to read config file: %v", err)
 	}
